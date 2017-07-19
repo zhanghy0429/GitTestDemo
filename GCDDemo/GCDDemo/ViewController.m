fd11c800bf8f4db5d257aca672d86064454796ba
zhanghy0429 <zhanghongyu126126@163.com>
2017-07-19 11:03:16 +0800

    更改测试
    更改测试了

--- GCDDemo/GCDDemo/ViewController.m
+++ GCDDemo/GCDDemo/ViewController.m
@@ -98,7 +98,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    // 好尴尬的测试
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         
         dispatch_async(dispatch_get_main_queue(), ^{
